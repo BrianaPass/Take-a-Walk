d74f40a6c7d50b3f2bf924bd60849677b0bff542
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2022-12-15 13:49:38 -0500

    added a google maps API with a unique key

---
+++ script.js
@@ -0,0 +1,3 @@
+// Gavin made a google map API request with a key:::
+
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDEcXU6u6I2VVF1Ir5oGx0oR_0TBc2xGI&callback=initMap"></script>
