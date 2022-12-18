9e4db4566802cb6677778d6ffd5c9856c4201d88
BrianaPass <116980413+BrianaPass@users.noreply.github.com>
2022-12-18 15:50:19 -0500

    Added google maps api, set up spotifify api

--- script.js
+++ script.js
@@ -1,16 +1,64 @@
+var url = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBDEcXU6u6I2VVF1Ir5oGx0oR_0TBc2xGI&callback=initMap&libraries=geometry,places"
+/*axios url = ""*/
+fetch(url)
+.then((response) => response.json())
+.then((data) => console.log(data))
 
+var data1 = data 
+/*Spotify API Key will go below*?*/
+var url = ""
+/*axios url = ""*/
+fetch(url)
+.then((response) => response.json())
+.then((data) => console.log(data))
+function getMusic(){var text = document.getElementById("alltext").value}
+/*Getting User Input*/
 
+/*https request*/
+var url = "https://routes.googleapis.com/directions/v2:computeRoutes"
+/*axios url = ""*/
+fetch(url)
+.then((response) => response.json())
+.then((data) => console.log(data))
 
+/*Generate route*/
+var url = "https://routes.googleapis.com/distanceMatrix/v2:computeRouteMatrix"
+/*axios url = ""*/
+fetch(url)
+.then((response) => response.json())
+.then((data) => console.log(data))
 
+var short = [5,10]
+var medium = [15,20]
+var long = [45,60]
+var random = [5,60]
+function short(){var text = document.getElementById("").value}
+function medium(){var text = document.getElementById("").value}
+function long(){var text = document.getElementById("").value}
+/* When I define short,medium, long in html add it into quotes
+This is the walk times*/
+initMap()
+const StartingEndingLocation = {lat:40.8075, lng:73.9626}
+var MorningsidePark = {lat:40.8061, lng:73.9585}
+var RiverSideBirdSanctuary = {lat:40.8103, lng:73.9659}
+var ConservatoryGarden = {lat:40.7937, lng:73.9525}
+var RandomLocation = Math.random()*4
+function constant() 
+var config = {
+  method: 'get',
+  url: 'StartingEndingLocation',
+  headers: { }
+};
 
-
-
-
-
-
-
-
-
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+/* Copy playlist button*/
+function GetSpotifyUrl(url){window.open(url)()}
 
 
 
@@ -84,5 +132,30 @@ function addMarker(location, map) {
     map: map,
   });
 }
-
+/*
 window.initMap = initMap;
+var object = 
+{
+  "origin": {
+    object (Waypoint)
+  },
+  "destination": {
+    object (Waypoint)
+  },
+  "intermediates": [
+    {
+      object (Waypoint)
+    }
+  ],
+  "travelMode": WALK (RouteTravelMode),
+  "departureTime": string,
+  "computeAlternativeRoutes": boolean,
+  "routeModifiers": {
+    object (RouteModifiers)
+  },
+  "languageCode": string,
+  "units": IMPERIAL,
+  "requestedReferenceRoutes": [
+    enum (ReferenceRoute)
+  ]
+}*/
