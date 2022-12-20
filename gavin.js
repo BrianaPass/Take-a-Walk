f40e6add24bd4066073d9457006c1c41a1ad2c63
RealGavinPrice <price.gavin@gmail.com>
2022-12-19 21:22:28 -0500

    click event vars for replacing src in iframe element
    3 vars to add in to Js

---
+++ gavin.js
@@ -0,0 +1,12 @@
+
+var srcShort = 
+'https://www.google.com/maps/embed/v1/directions?key=AIzaSyBDEcXU6u6I2VVF1Ir5oGx0oR_0TBc2xGI&origin=' + StartingEndingLocation + '&waypoints=' + MorningsidePark + '&destination=' + StartingEndingLocation;
+
+var srcMedium = 
+'https://www.google.com/maps/embed/v1/directions?key=AIzaSyBDEcXU6u6I2VVF1Ir5oGx0oR_0TBc2xGI&origin=' + StartingEndingLocation + '&waypoints=' + RiverSideBirdSanctuary + '&destination=' + StartingEndingLocation;
+
+var srcLong = 
+'https://www.google.com/maps/embed/v1/directions?key=AIzaSyBDEcXU6u6I2VVF1Ir5oGx0oR_0TBc2xGI&origin=' + StartingEndingLocation +  '&waypoints=' + ConservatoryGarden + '&destination=' + StartingEndingLocation;
+
+
+
